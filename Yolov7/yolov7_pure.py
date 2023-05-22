b66042e726b52854de3274eb5f64ba09bc9810c6
fireblue95 <128771832+fireblue95@users.noreply.github.com>
2023-05-22 16:45:27 +0800

    Update yolov7_pure.py

--- Yolov7/yolov7_pure.py
+++ Yolov7/yolov7_pure.py
@@ -1,84 +1,180 @@
 import cv2
 import time
 import torch
-from pathlib import Path
-from typing import Tuple
-from warnings import filterwarnings
-
+import argparse
+import torchvision
+import numpy as np
+from numpy import random
 from models.experimental import attempt_load
 
-filterwarnings('ignore')
+def scale_coords(img1_shape, coords, img0_shape, ratio_pad=None):
+    # Rescale coords (xyxy) from img1_shape to img0_shape
+    if ratio_pad is None:  # calculate from img0_shape
+        gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  # gain  = old / new
+        pad = (img1_shape[1] - img0_shape[1] * gain) / 2, (img1_shape[0] - img0_shape[0] * gain) / 2  # wh padding
+    else:
+        gain = ratio_pad[0][0]
+        pad = ratio_pad[1]
 
-import numpy as np
+    coords[:, [0, 2]] -= pad[0]  # x padding
+    coords[:, [1, 3]] -= pad[1]  # y padding
+    coords[:, :4] /= gain
+    clip_coords(coords, img0_shape)
+    return coords
 
-class Yolov7_pure:
-    def __init__(self, yolov7_conf: float = 0.5):
-        
-        # -----------------------------------------------------------------
-        # load model
+def clip_coords(boxes, img_shape):
+    # Clip bounding xyxy bounding boxes to image shape (height, width)
+    boxes[:, 0].clamp_(0, img_shape[1])  # x1
+    boxes[:, 1].clamp_(0, img_shape[0])  # y1
+    boxes[:, 2].clamp_(0, img_shape[1])  # x2
+    boxes[:, 3].clamp_(0, img_shape[0])  # y2
 
-        self.yolov7_conf = yolov7_conf
+def box_iou(box1, box2):
+    # https://github.com/pytorch/vision/blob/master/torchvision/ops/boxes.py
+    """
+    Return intersection-over-union (Jaccard index) of boxes.
+    Both sets of boxes are expected to be in (x1, y1, x2, y2) format.
+    Arguments:
+        box1 (Tensor[N, 4])
+        box2 (Tensor[M, 4])
+    Returns:
+        iou (Tensor[N, M]): the NxM matrix containing the pairwise
+            IoU values for every element in boxes1 and boxes2
+    """
 
-        self.yolo_weights = Path('./yolov7.pt')
+    def box_area(box):
+        # box = 4xn
+        return (box[2] - box[0]) * (box[3] - box[1])
 
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    area1 = box_area(box1.T)
+    area2 = box_area(box2.T)
 
-        self.model = attempt_load(self.yolo_weights, map_location=self.device)  # load FP32 model
-        self.names = self.model.module.names if hasattr(self.model, 'module') else self.model.names  # Get the class names
+    # inter(N,M) = (rb(N,M,2) - lt(N,M,2)).clamp(0).prod(2)
+    inter = (torch.min(box1[:, None, 2:], box2[:, 2:]) - torch.max(box1[:, None, :2], box2[:, :2])).clamp(0).prod(2)
+    return inter / (area1[:, None] + area2 - inter)  # iou = inter / (area1 + area2 - inter)
 
+def xywh2xyxy(x):
+    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
+    y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
+    y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
+    y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+    return y
 
-        # load model
-        # -----------------------------------------------------------------
+def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=None, agnostic=False, multi_label=False,
+                        labels=()):
+    """Runs Non-Maximum Suppression (NMS) on inference results
 
-        # -----------------------------------------------------------------
-        # Set configure
+    Returns:
+         list of detections, on (n,6) tensor per image [xyxy, conf, cls]
+    """
 
-        self.win_title = 'Yolov7'
+    nc = prediction.shape[2] - 5  # number of classes
+    xc = prediction[..., 4] > conf_thres  # candidates
 
-        # Set configure
-        # -----------------------------------------------------------------
+    # Settings
+    min_wh, max_wh = 2, 4096  # (pixels) minimum and maximum box width and height
+    max_det = 300  # maximum number of detections per image
+    max_nms = 30000  # maximum number of boxes into torchvision.ops.nms()
+    time_limit = 10.0  # seconds to quit after
+    redundant = True  # require redundant detections
+    multi_label &= nc > 1  # multiple labels per box (adds 0.5ms/img)
+    merge = False  # use merge-NMS
 
+    t = time.time()
+    output = [torch.zeros((0, 6), device=prediction.device)] * prediction.shape[0]
+    for xi, x in enumerate(prediction):  # image index, image inference
+        # Apply constraints
+        # x[((x[..., 2:4] < min_wh) | (x[..., 2:4] > max_wh)).any(1), 4] = 0  # width-height
+        x = x[xc[xi]]  # confidence
 
-    def detector(self, frame: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-        """
-        Yolov7 detect the frame and send back the result.
+        # Cat apriori labels if autolabelling
+        if labels and len(labels[xi]):
+            l = labels[xi]
+            v = torch.zeros((len(l), nc + 5), device=x.device)
+            v[:, :4] = l[:, 1:5]  # box
+            v[:, 4] = 1.0  # conf
+            v[range(len(l)), l[:, 0].long() + 5] = 1.0  # cls
+            x = torch.cat((x, v), 0)
 
-        The boxes is (640, 640). scales is 0 ~ 640
-            type is [xmin, ymin, width, height]
-            than will return boxes type:
-                (0 ~ ori_w, 0 ~ ori_h)
-                [xmin, ymin, xmax, ymax]
-        """
+        # If none remain process next image
+        if not x.shape[0]:
+            continue
 
-        ori_h, ori_w, _ = frame.shape
+        # Compute conf
+        if nc == 1:
+            x[:, 5:] = x[:, 4:5] # for models with one class, cls_loss is 0 and cls_conf is always 0.5,
+                                 # so there is no need to multiplicate.
+        else:
+            x[:, 5:] *= x[:, 4:5]  # conf = obj_conf * cls_conf
 
-        # Convert the ROI image to a PyTorch tensor and move it to the GPU
-        blob = torch.from_numpy(cv2.resize(frame, (640, 640))).to(self.device)
-        blob = blob.permute(2, 0, 1).float().unsqueeze(0) / 255.0
-        
-        # Detect objects in the ROI using the YOLOv7 model
-        detections = self.model(blob)[0][0]
+        # Box (center x, center y, width, height) to (x1, y1, x2, y2)
+        box = xywh2xyxy(x[:, :4])
 
-        # Keep only detections with a confidence score > 0.5
-        detections = detections[detections[:, 4] > self.yolov7_conf]
+        # Detections matrix nx6 (xyxy, conf, cls)
+        if multi_label:
+            i, j = (x[:, 5:] > conf_thres).nonzero(as_tuple=False).T
+            x = torch.cat((box[i], x[i, j + 5, None], j[:, None].float()), 1)
+        else:  # best class only
+            conf, j = x[:, 5:].max(1, keepdim=True)
+            x = torch.cat((box, conf, j.float()), 1)[conf.view(-1) > conf_thres]
 
-        boxes, scores, class_ids = detections[..., :4], detections[..., 4], detections[..., 5:]
+        # Filter by class
+        if classes is not None:
+            x = x[(x[:, 5:6] == torch.tensor(classes, device=x.device)).any(1)]
 
-        if boxes.shape[0] == 0:
-            return np.array([]), np.array([]), np.array([])
+        # Apply finite constraint
+        # if not torch.isfinite(x).all():
+        #     x = x[torch.isfinite(x).all(1)]
 
-        class_ids = torch.argmax(class_ids, dim=-1)
+        # Check shape
+        n = x.shape[0]  # number of boxes
+        if not n:  # no boxes
+            continue
+        elif n > max_nms:  # excess boxes
+            x = x[x[:, 4].argsort(descending=True)[:max_nms]]  # sort by confidence
 
-        xmin, ymin, w, h = boxes[..., 0], boxes[..., 1], boxes[..., 2], boxes[..., 3]
+        # Batched NMS
+        c = x[:, 5:6] * (0 if agnostic else max_wh)  # classes
+        boxes, scores = x[:, :4] + c, x[:, 4]  # boxes (offset by class), scores
+        i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
+        if i.shape[0] > max_det:  # limit detections
+            i = i[:max_det]
+        if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
+            # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
+            iou = box_iou(boxes[i], boxes) > iou_thres  # iou matrix
+            weights = iou * scores[None]  # box weights
+            x[i, :4] = torch.mm(weights, x[:, :4]).float() / weights.sum(1, keepdim=True)  # merged boxes
+            if redundant:
+                i = i[iou.sum(1) > 1]  # require redundancy
 
-        o_xmin = xmin / 640 * ori_w
-        o_ymin = ymin / 640 * ori_h
-        o_xmax = (xmin + w) / 640 * ori_w
-        o_ymax = (ymin + h) / 640 * ori_h
+        output[xi] = x[i]
+        if (time.time() - t) > time_limit:
+            print(f'WARNING: NMS time limit {time_limit}s exceeded')
+            break  # time limit exceeded
 
-        out_boxes = torch.stack([o_xmin, o_ymin, o_xmax, o_ymax], axis=-1)
+    return output
 
-        return out_boxes.cpu().numpy(), scores.cpu().numpy(), class_ids.cpu().numpy()
+class Yolov7:
+    def __init__(self, opt):
+
+        self.opt = opt
+
+        # Configurations ------------------------------------------------------------------------------------------------
+
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.half = self.device.type != 'cpu'  # half precision only supported on CUDA
+
+        self.model = attempt_load(self.opt.weights, map_location=self.device)  # load FP32 model
+
+        if self.half:
+            self.model.half()  # to FP16
+
+        self.names = self.model.module.names if hasattr(self.model, 'module') else self.model.names
+        self.colors = [[random.randint(0, 255) for _ in range(3)] for _ in self.names]
+
+        # Configurations ------------------------------------------------------------------------------------------------
 
     def draw_detections(self, image, class_names, boxes, scores, class_ids, mask_alpha=0.3):
         rng = np.random.default_rng(3)
@@ -122,35 +218,72 @@ class Yolov7_pure:
 
         return cv2.addWeighted(mask_img, mask_alpha, det_img, 1 - mask_alpha, 0)
 
-    @torch.no_grad()
-    def detect_objects_realtime(self):
-        # Start the video capture
-        cap = cv2.VideoCapture(0)
+    def predict(self, frame_):
+        frame = cv2.cvtColor(frame_, cv2.COLOR_BGR2RGB)
+
+        blob = cv2.resize(frame, (640, 384))
+        img = torch.from_numpy(blob).to(self.device)
+        img = img.half() if self.half else img.float()  # uint8 to fp16/32
+        img = (img / 255.0).unsqueeze(0).permute(0, 3, 1, 2)  # 0 - 255 to 0.0 - 1.0
+
+        with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
+            pred = self.model(img, augment=self.opt.augment)[0]
+        return img.shape, pred
+
+    def detector(self, frame):
+
+        img_shape, pred = self.predict(frame)
+
+        # Apply NMS
+        pred = non_max_suppression(pred, self.opt.conf_thres, self.opt.iou_thres, classes=self.opt.classes, agnostic=self.opt.agnostic_nms)[0]
         
+        if len(pred) > 0:
+            # Rescale boxes from img_size to im0 size
+            pred[:, :4] = scale_coords(img_shape[2:], pred[:, :4], frame.shape).round()
+
+            return pred[..., :4].detach().cpu().numpy(), pred[..., 4].detach().cpu().numpy(), pred[..., 5].detach().cpu().numpy().astype(np.int32)
+        return np.array([]), np.array([]), np.array([])
+
+    def run(self):
+        cap = cv2.VideoCapture(self.opt.source)
         while True:
-            success, frame = cap.read()
+            success, frame_ = cap.read()
 
             if not success:
                 break
 
-            frame_drew = frame.copy()
-            boxes, scores, class_ids = self.detector(frame)
+            boxes, scores, class_ids = self.detector(frame_)
 
-            frame_drew = self.draw_detections(frame_drew, self.names, boxes, scores, class_ids)
+            drawed_frame = self.draw_detections(frame_, self.names, boxes, scores, class_ids)
 
-            # Display the ROI image with bounding boxes
-            cv2.imshow(self.win_title, frame_drew)
-            
-            # Wait for a key press to exit
-            if cv2.waitKey(1) == ord('q'):
+            cv2.imshow('test', drawed_frame)
+            key = cv2.waitKey(1)  # 1 millisecond
+            if key == 27 or key == ord('q'):  # ESC
                 break
 
-        # Release the video capture and close all windows
-        cap.release()
-        cv2.destroyAllWindows()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.45, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
+    opt = parser.parse_args()
+    print(opt)
 
+    app = Yolov7(opt)
+    app.run()
     
-
-if __name__ == "__main__":
-    app: Yolov7_pure = Yolov7_pure()
-    app.detect_objects_realtime()
